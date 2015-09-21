fb21a56cd0f1b2912abe8173d72a5f594c9af9aa
miguel <miguel.alarcos@gmail.com>
2015-09-21 13:14:22 +0200

    preparing for integration manual tests (v2)

--- components/main/main.py
+++ components/main/main.py
@@ -2,7 +2,7 @@ import sys
 sys.path.insert(0, 'http://localhost:8888/components')
 import random
 print('Iniciando aplicación::', random.random())
-
+from components.main.models.A import A
 from components.main.init import init
 
 init()
@@ -15,5 +15,30 @@ from components.main.controller import Controller
 key = [('x', 'desc')]
 filter = ('my_filter', {'x': 5, 'y': 10})
 Controller('MyController', key, filter)
+
+button_send = jq('#button')
+sent_initial_data = False
+
+
+def send_data():
+    global sent_initial_data
+    if not sent_initial_data:
+        Controller.subscribe_all()
+        sent_initial_data = True
+    try:
+        if random.random() < 0.5:
+            obj = A(None, x=random.randint(0, 10))
+        else:
+            obj = random.choice(list(A.objects.values()))
+            print('*** random choice object')
+            obj.x = random.randint(0, 10)
+    except Exception as e:
+        print ('-----------error:', e)
+        obj = A(None, x=random.randint(0, 10))
+    obj.save()
+
+
+button_send.bind('click', send_data)
+
 
 
--- components/main/models/A.py
+++ components/main/models/A.py
@@ -4,7 +4,7 @@ from components.main.reactive import Model, register
 @register
 class A(Model):
     def validate_x(self):
-        return type(self.x) == int and x > 100
+        return type(self.x) == int and self.x > 0
 
 
 

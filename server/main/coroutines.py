81bea218003dbe7c56515cde87fff70a56bbc00c
miguel <miguel.alarcos@gmail.com>
2015-09-23 09:02:24 +0200

    handle_collection

--- server/main/coroutines.py
+++ server/main/coroutines.py
@@ -50,85 +50,100 @@ def handle_filter(item):
     if len(ret) > 0:
         ret = [(client.socket, r) for r in ret]
         yield q_send.put(ret)
-    raise gen.Return('handle_filter done')
+    #raise gen.Return('handle_filter done')
+    return # ver si es necesario un argumento a retornar
+
+
+def handle_rpc(item):
+    name = item.pop('__RPC__')
+    task = registered_tasks[name]
+    ioloop.IOLoop.current().spawn_callback(task, db=DB(db), queue=q_mongo, **item)
+
+
+@gen.coroutine
+def handle_collection(item):
+    collection = item['__collection__']
+    del item['__client__']
+    model = item
+
+    new = False
+    deleted = False
+    print('buscamos model before')
+    model_before = yield db[collection].find_one({'_id': model['id']})
+    print('model before', model_before)
+    if model_before is None:
+        model_id = model.copy()
+        model_id['_id'] = model_id['id']
+        del model_id['id']
+        del model_id['__collection__']
+
+        if validate(model):
+            print('yield insert', model_id)
+            yield db[collection].insert(model_id)
+        else:
+            print('retornamos sin hacer insert')
+            return
+        new = True
+    elif '__deleted__' in model.keys():
+        deleted = True
+        print('yield remove')
+        yield db[collection].remove({'_id': model['id']})
+    else:
+        model_copy = model.copy()
+        del model_copy['id']
+        del model_copy['__collection__']
+        print('yield update')
+        model2validate = model_before.copy()
+        model2validate.update(model)
+        if validate(model2validate):
+            yield db[collection].update({'_id': model['id']}, {"$set": model_copy})
+        else:
+            return
+    yield broadcast(collection, new, model_before, deleted, model)
+
+
+@gen.coroutine
+def broadcast(collection, new, model_before, deleted, model):
+    for client in Client.clients.values():
+        print('filter of client:', client.filters)
+        for filt in client.filters.values():
+            print('filter:', filt)
+            if filt['__collection__'] != collection:
+                continue
+            before = (not new) and pass_filter(filt, model_before)
+            print('before:', before)
+
+            if not before and not deleted:
+                if model_before is None:    # repasar esto
+                    model_after = model.copy()
+                else:
+                    model_after = model_before.copy()
+                    model_after.update(model)
+                after = pass_filter(filt, model_after)
+                print('after:', after)
+                if after:
+                    print('send', client.socket, model)
+                    yield q_send.put((client.socket, model))
+                    break
+            else:
+                print('send', client.socket, model)
+                yield q_send.put((client.socket, model))
+                break
 
 
 @gen.coroutine
 def mongo_consumer():
-
     while True:
-        print('yield: q_mongo.get()')
         item = yield q_mongo.get()
         print('item from queue', item)
 
         if '__filter__' in item.keys():
             yield handle_filter(item)
         elif '__RPC__' in item.keys():
-            name = item.pop('__RPC__')
-            task = registered_tasks[name]
-            ioloop.IOLoop.current().spawn_callback(task, db=DB(db), queue=q_mongo, **item)
+            handle_rpc(item)
         else:
-            collection = item['__collection__']
-            del item['__client__']
-            model = item
+            yield handle_collection(item)
 
-            new = False
-            deleted = False
-            print('buscamos model before')
-            model_before = yield db[collection].find_one({'_id': model['id']})
-            print('model before', model_before)
-            if model_before is None:
-                model_id = model.copy()
-                model_id['_id'] = model_id['id']
-                del model_id['id']
-                del model_id['__collection__']
-                print('yield insert', model_id)
-                if validate(model):
-                    yield db[collection].insert(model_id)
-                else:
-                    continue
-                new = True
-            elif '__deleted__' in model.keys():
-                deleted = True
-                print('yield remove')
-                yield db[collection].remove({'_id': model['id']})
-            else:
-                model_copy = model.copy()
-                del model_copy['id']
-                del model_copy['__collection__']
-                print('yield update')
-                model2validate = model_before.copy()
-                model2validate.update(model)
-                if validate(model2validate):
-                    yield db[collection].update({'_id': model['id']}, {"$set": model_copy})
-                else:
-                    continue
-
-            for client in Client.clients.values():
-                print('filter of client:', client.filters)
-                for filt in client.filters.values():
-                    print('filter:', filt)
-                    if filt['__collection__'] != collection:
-                        continue
-                    before = (not new) and pass_filter(filt, model_before)
-                    print('before:', before)
-
-                    if not before and not deleted:
-                        if model_before is None:    # repasar esto
-                            model_after = model.copy()
-                        else:
-                            model_after = model_before.copy()
-                            model_after.update(model)
-                        after = pass_filter(filt, model_after)
-                        print('after:', after)
-                        if after:
-                            print('send', client.socket, model)
-                            yield q_send.put((client.socket, model))
-                            break
-                    else:
-                        print('send', client.socket, model)
-                        yield q_send.put((client.socket, model))
-                        break
         q_mongo.task_done()
 
 
